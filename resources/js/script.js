ee7524cb49c920d6159827fce91a3a24af2ac0b4
Akhmad Raihan Aulia Fikri <114968073+Akmdrhannn@users.noreply.github.com>
2023-02-27 17:31:41 +0700

    Create script.js

---
+++ resources/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("boo").addEventListener("click",myFunction);
+var img='<img src="../css/asset/heart.png" alt="heart">';
+function myFunction(){
+    document.getElementById("boo").innerHTML = 'Thanks for visiting my website . Hope your day always getting good news.<img src="../css/asset/heart.png" alt="heart">';
+}
