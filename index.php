8398d0b1728361578eabe3568ef262f0946edcf3
Akhmad Raihan Aulia Fikri <114968073+Akmdrhannn@users.noreply.github.com>
2023-02-27 17:22:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="resources/css/asset/akmdrhan.png">
+    <link rel="stylesheet" href="resources/css/style.css">
+    <title>Akmdrhan</title>
+</head>
+<body>
+<section class="layout">
+  <div class="header">
+    <!-- header kiri -->
+    <div class="header-left"> 
+    <div class="bg-image-hover-zoom1">
+    <div class="dropdown show">
+      <a class="btn btn-secondary dropdown-toggle" style="background: none;border:none;" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img id="menu" src="resources/css/asset/menu.png" alt=""></a>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item" href="index.php">Home</a>
+    <a class="dropdown-item" href="#">Portofolio</a>
+    <a class="dropdown-item" href="#">Certificate</a>
+    <a class="dropdown-item" href="resources/view/profile.php">Profile</a>
+      </div>
+    </div>
+    </div>
+      <img id="akmdrhan" src="resources/css/asset/akmdrhan.png" alt="akmdrhan">
+    </div>
+    <!-- header kanan -->
+    <div class="header-right"> 
+      <a href="https://www.linkedin.com/in/akmdrhan/">
+        <div class="bg-image-hover-zoom">
+          <img id="linkdn" src="resources/css/asset/linkedin.png" alt="linkdn">
+        </div>
+      </a>
+      <a href="https://github.com/Akmdrhannn">
+      <div class="bg-image-hover-zoom">
+        <img id="github" src="resources/css/asset/github.png" alt="github">
+      </div>  
+      </a>
+    </div>
+  </div>
+
+  <!-- Konten utama -->
+  <div class="main">
+  <div class="row">
+      <div class="col text-center mb-5">
+         <h1 class="display-4">Be 1% people and Escape the Matrix</h1><br>
+      <p class="lead">Hello and greeting. I'm Raihan and i always try to keep grinding into my goals to be 'Full Stack Web Developer' from Indonesia</p>
+      </div>
+    </div>
+  <div class="container">
+  <!-- card -->
+  <div class="row">
+    <!-- card pertama -->
+  <div class="col-sm-12 col-md-6 col-lg-4 mb-4"><div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?computer,design');">
+         <img class="card-img d-none" src="https://source.unsplash.com/600x900/?computer,design">
+        <div class="card-img-overlay d-flex flex-column">
+         <div class="card-body">
+            <h4 class="card-title mt-0 "><a class="text-white" href="resources/view/portofolio.php">My Portofolio</a></h4>
+           <small> Check out my portofolio here</small>
+          </div>
+          <div class="card-footer">
+           <div class="media">
+  <img class="mr-3 rounded-circle" src="resources/css/asset/portfolio.png" alt="Portofolio" style="max-width:50px">
+</div>
+</div>
+      </div>
+      </div></div>
+
+      <!-- card kedua -->
+     <div class="col-sm-12 col-md-6 col-lg-4 mb-4"><div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?computer,certificate');">
+         <img class="card-img d-none" src="https://source.unsplash.com/600x900/?computer,certificate" >
+         <div class="card-img-overlay d-flex flex-column">
+         <div class="card-body">
+            <h4 class="card-title mt-0 "><a class="text-white" href="resources/view/certificate.php">Certificate</a></h4>
+           <small> You can see what i've got when i study :D </small>
+          </div>
+          <div class="card-footer">
+           <div class="media">
+  <img class="mr-3 rounded-circle" src="resources/css/asset/certificate.png" alt="certificate" style="max-width:50px">
+</div>
+</div>
+      </div>
+      </div></div>
+      <!-- card ketiga -->
+  <div class="col-sm-12 col-md-6 col-lg-4 mb-4"><div class="card text-white card-has-bg click-col" style="background-image:url('https://source.unsplash.com/600x900/?profile,myself');">
+         <img class="card-img d-none" src="https://source.unsplash.com/600x900/?profile,myself" >
+         <div class="card-img-overlay d-flex flex-column">
+         <div class="card-body">
+            <h4 class="card-title mt-0 "><a class="text-white" href="resources/view/profile.php">Profile</a></h4>
+           <small> Let me bring you to know me. Come here :)</small>
+          </div>
+          <div class="card-footer">
+           <div class="media">
+  <img class="mr-3 rounded-circle" src="resources/css/asset/profile.png" alt="profile" style="max-width:50px">
+</div>
+</div>
+      </div>
+      </div></div>
+
+</div>
+  </div>
+  </div>
+
+
+  
+  <!-- footer -->
+  <div class="footer">
+    <footer  class="bg-dark text-center text-lg-start text-white" >
+      <div class="container d-flex justify-content-center py-5">
+        <div class="bg-image-hover-zoom">
+          <button type="button" class="btn btn-secondary btn-lg btn-floating mx-2" style="background-color: #1C1C1C;border-radius: 50px;">
+            <a href="mailto:akh.raihanaf@gmail.com"><img src="resources/css/asset/gmail.png"></a>
+          </button>
+        </div>
+        <div class="bg-image-hover-zoom">
+          <button type="button" class="btn btn-secondary btn-lg btn-floating mx-2" style="background-color: #1C1C1C;border-radius: 50px;">
+            <a href="https://www.instagram.com/akmdrhannn_/"><img src="resources/css/asset/instagram.png"></a>
+          </button>
+      </div>
+      <div class="bg-image-hover-zoom">
+        <button type="button" class="btn btn-secondary btn-lg btn-floating mx-2" style="background-color: #1C1C1C;border-radius: 50px;">
+          <a style="color: black;text-decoration:none;" href="https://wa.me/6282234086611"><img src="resources/css/asset/whatsapp.png"><span>(Chat Only)</span></a>       
+        </button>
+      </div>
+    </div>
+
+      <!-- Copyright -->
+      <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2023 Copyright:
+        <a class="text-white" href="#">Akmdrhan</a>
+      </div>
+    </footer>
+  </div>
+</section>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
